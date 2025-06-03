c432763c41228896d4f587487a25b78a3bc0f5c6
saeed <saeed.mo@gmail.com>
2025-06-03 16:15:51 +0330

    update

--- DisposableEmailChecker.TestApp/Program.cs
+++ DisposableEmailChecker.TestApp/Program.cs
@@ -8,22 +8,25 @@ while (true)
     if (string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase))
         break;
 
-    var result = DisposableEmailDomains.CheckEmail(input);
-
-    switch (result)
+    if (input != null)
     {
-        case EmailCheckResult.IsEmpty:
-            Console.WriteLine("⚠️ Email is empty.");
-            break;
-        case EmailCheckResult.InvalidFormat:
-            Console.WriteLine("❌ Invalid email format.");
-            break;
-        case EmailCheckResult.IsDisposable:
-            Console.WriteLine("⚠️ IsDisposable email detected.");
-            break;
-        case EmailCheckResult.IsValid:
-            Console.WriteLine("✅ Email is valid and not disposable.");
-            break;
+        var result = DisposableEmailDomains.CheckEmail(input);
+
+        switch (result)
+        {
+            case EmailCheckResult.IsEmpty:
+                Console.WriteLine("⚠️ Email is empty.");
+                break;
+            case EmailCheckResult.InvalidFormat:
+                Console.WriteLine("❌ Invalid email format.");
+                break;
+            case EmailCheckResult.IsDisposable:
+                Console.WriteLine("⚠️ IsDisposable email detected.");
+                break;
+            case EmailCheckResult.IsValid:
+                Console.WriteLine("✅ Email is valid and not disposable.");
+                break;
+        }
     }
 
     Console.WriteLine(); // Add a blank line for readability
